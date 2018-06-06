854a88404f0a94a24e725ebe8a8783290295a6d7
Higor Senna CHaves <s2it_hchaves@uolinc.com>
2018-06-06 15:28:20 -0300

    Retirado comentarios

--- Trabalho/mysql-start.sh
+++ Trabalho/mysql-start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Start the first process
 service mysql start &&
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');flush privileges;update mysql.user set plugin=null where user='root';flush privileges" && mysql -u root -proot -e "source /opt/GLPI.sql"
 
@@ -9,12 +8,6 @@ if [ -z "$status" ]; then
   echo "Falha ao iniciar o mysql: $status"
   exit $status
 fi
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container exits with an error
-# if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
 
 while sleep 60; do
   status=$(pgrep mysqld)
