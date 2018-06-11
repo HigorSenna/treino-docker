e8837522b8942fbf4c6336dab88e39dd578751ad
Higor Senna CHaves <s2it_hchaves@uolinc.com>
2018-06-11 09:31:23 -0300

    Adicionado condig de permissionamento e adicionado projeto .zip para evitar erro do composer

--- Trabalho/configurar-servicos.sh
+++ Trabalho/configurar-servicos.sh
@@ -6,8 +6,6 @@ service mysql start
 
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');flush privileges;update mysql.user set plugin=null where user='root';flush privileges;" && echo "restaurando banco.."
 
-sleep 10
-
 mysql -u root -proot -e "source /opt/GLPI.sql" 
 
 echo "iniciando apache"  && service apache2 start
@@ -20,10 +18,6 @@ fi
 
 while sleep 60; do
   status=$(pgrep mysqld)
-  # ps aux |grep my_second_process |grep -q -v grep
-  # PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
   if [ -z "$status" ]; then
     echo "O Processo Terminou."
     exit 1
